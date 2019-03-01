9d1f2c2eac67d17b6baee061970b95abaa53cf69
Hanyuu <HanyuuFurude@outlook.com>
2019-03-01 09:20:04 +0800

    201903010919

--- t.py
+++ t.py
@@ -4,6 +4,22 @@ import requests
 import json
 import sys
 import chardet
+import logging
+
+# debug() 调试级别，一般用于记录程序运行的详细信息
+# info() 事件级别，一般用于记录程序的运行过程
+# warnning() 警告级别，，一般用于记录程序出现潜在错误的情形
+# error() 错误级别，一般用于记录程序出现错误，但不影响整体运行
+# critical 严重错误级别 ， 出现该错误已经影响到整体运行
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(asctime)s] $(filename)s \n [line:%(lineno)d] %(levelname)s %(message)s',
+    datefmt='%Y/%b/%d %H:%M:%S',
+    filename='log.log',
+    filemode='a'
+)
+# 翻译
 
 
 def translate(queryString: str)->str:
@@ -24,26 +40,37 @@ def translate(queryString: str)->str:
     except Exception:
         return None
 
+# 从剪切板获取文本
 
-def gettext():
+
+def gettext()->str:
     try:
         w.OpenClipboard()
         t = w.GetClipboardData(win32con.CF_TEXT)
         w.CloseClipboard()
         t = t.decode('gbk')
+        logging.debug(str('[read from clipboard]:', t))
         return t
     except Exception as e:
-        print(e)
+        logging.error(str('can\'t read from clipboard.\n', e))
         return None
 
 
-
-def settext(aString):
+def settext(aString)->None:
     if aString is not None:
-        w.OpenClipboard()
-        w.EmptyClipboard()
-        w.SetClipboardData(win32con.CF_UNICODETEXT, aString)
-        w.CloseClipboard()
+        try:
+            w.OpenClipboard()
+            w.EmptyClipboard()
+            w.SetClipboardData(win32con.CF_UNICODETEXT, aString)
+            w.CloseClipboard()
+        except Exception as e:
+            logging.error(str('write to clipboard failure', e))
+            return None
+        finally:
+            try:
+                w.CloseClipboard()
+            except Exception as e:
+                logging.critical(str('close clipboard failure', e))
 
 
 if __name__ == "__main__":
--- ui.py
+++ ui.py
@@ -4,6 +4,16 @@ from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QCheckBox, QLabe
 from PyQt5.uic import loadUi
 from PyQt5.QtCore import QCoreApplication, QTimer
 import t
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(asctime)s] $(filename)s \n [line:%(lineno)d] %(levelname)s %(message)s',
+    datefmt='%Y/%b/%d %H:%M:%S',
+    filename='log.log',
+    filemode='a'
+)
+
 
 class ui(QMainWindow):
     def __init__(self):
@@ -19,13 +29,14 @@ class ui(QMainWindow):
         self.checkBoxR.stateChanged.connect(self.checkBoxChanged)
         self.checkBoxW = self.findChild(QCheckBox, 'checkBoxW')
         self.checkBoxW.stateChanged.connect(self.checkBoxChanged)
-        self.textRead = self.findChild(QPlainTextEdit,'textOrigin')
-        self.textWrite = self.findChild(QPlainTextEdit,'textTranslate')
-        self.buttonExit = self.findChild(QPushButton,'buttonExit')
-        self.buttonExit.clicked.connect(QCoreApplication.exit)  # button on exit
-        self.timer = QTimer(self) #初始化一个定时器
-        self.timer.timeout.connect(self.translate) #计时结束调用operate()方法
-        self.timer.start(1000) #设置计时间隔并启动
+        self.textRead = self.findChild(QPlainTextEdit, 'textOrigin')
+        self.textWrite = self.findChild(QPlainTextEdit, 'textTranslate')
+        self.buttonExit = self.findChild(QPushButton, 'buttonExit')
+        self.buttonExit.clicked.connect(
+            QCoreApplication.exit)  # button on exit
+        self.timer = QTimer(self)  # 初始化一个定时器
+        self.timer.timeout.connect(self.translate)  # 计时结束调用operate()方法
+        self.timer.start(1000)  # 设置计时间隔并启动
         self.show()
 
     # checkBox function
@@ -34,25 +45,16 @@ class ui(QMainWindow):
             self.autoRead = True if(state == 2) else False
         elif self.sender() == self.checkBoxW:
             self.autoWrite = True if (state == 2) else False
-        # print('[auto read]', self.autoRead)
-        # print('[suto write]', self.autoWrite)
-        # print(self.textRead.toPlainText())
-        # self.textWrite.clear()
-        # self.textWrite.appendPlainText(self.textRead.toPlainText())
-        # print(a.text())
-        # a.text('a')
-    # get the input box
 
     # fetch the text from textOrigin
     def fetchPlainText(self):
         return self.textRead.toPlainText()
 
     # set the text to textTranslate
-    def setPlainText(self,text):
+    def setPlainText(self, text):
         self.textWrite.clear()
         self.textWrite.appendPlainText(text)
         return text
-
 
     def translate(self):
         if self.autoRead is True:
@@ -67,22 +69,23 @@ class ui(QMainWindow):
             return
         else:
             self.text = text
-        print('text:',text)
         if text is None:
             self.setPlainText('[empty]')
             return
-        elif text==self.res:
+        elif text == self.res:
             return
         self.res = self.setPlainText(t.translate(text))
-        print('res', self.res)
         self.setPlainText(self.res)
         if self.autoWrite is True:
             self.writeClipboard(self.res)
 
     def readClipboard(self):
         return t.gettext()
-    def writeClipboard(self,text):
+
+    def writeClipboard(self, text):
         t.settext(text)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     run = ui()
